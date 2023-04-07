3fb688c94323da537643db7dd9abf326dcb0daa2
oleksandrmaliy <115729512+oleksandrmaliy@users.noreply.github.com>
2023-04-07 12:41:03 +0300

    part 1 is ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryCollection = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link" href="large-image.jpg"><img class="gallery__image" src=${preview} data-source=${original} alt=${description}/></a></li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', galleryCollection);
+
+gallery.addEventListener('click', onOpenModal);
+
+function onOpenModal(event) {
+  event.preventDefault();
+
+  const currentItem = event.target;
+
+  if (currentItem.nodeName !== 'IMG') {
+    return;
+  }
+
+  const modalWindow = basicLightbox.create(`
+   <img class="gallery__image" src="${currentItem.dataset.source}" width="800" height="600">
+`);
+
+  modalWindow.show();
+
+  window.addEventListener('keydown', onCloseModal);
+}
+
+function onCloseModal(event) {
+  const openModal = document.querySelector('.basicLightbox');
+
+  if (event.code === 'Escape') {
+    openModal.remove();
+  }
+
+  window.removeEventListener('keydown', onCloseModal);
+}
+
+// console.log(galleryItems);
